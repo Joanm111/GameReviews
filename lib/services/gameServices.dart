9adcd98bd2395205f847d34b11ebbb4c3d81a524
joan <joanjorge42@gmail.com>
2022-12-05 18:48:48 -0400

    2g

--- lib/services/gameServices.dart
+++ lib/services/gameServices.dart
@@ -2,4 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class gameServices extends ChangeNotifier {}
+class gameServices extends ChangeNotifier {
+  gameServices() {
+    this.getOndisplayGame();
+  }
+
+  getOndisplayGame() {
+    print("...");
+  }
+}
